8f7d17248d58855032230cbd10e79e9a9025cd84
Jo Van den Berghe <jo.vandenberghe@smartphoto.com>
2026-03-25 19:30:58 +0100

    github pages

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from "@solidjs/start/config";
 
+const serverPreset = process.env.SERVER_PRESET;
+const serverBaseURL = process.env.SERVER_BASE_URL;
+
 export default defineConfig({
   ssr: false,
+  vite: {
+    server: {
+      port: 3000,
+    },
+  },
   server: {
-    preset: "github-pages",
+    ...(serverPreset ? { preset: serverPreset } : {}),
+    ...(serverBaseURL ? { baseURL: serverBaseURL } : {}),
   },
 });
