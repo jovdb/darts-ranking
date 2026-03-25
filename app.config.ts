191b8685fa6fbd66d43a09887cdc341dff9891c1
Jo Van den Berghe <jo.vandenberghe@smartphoto.com>
2026-03-25 18:19:14 +0100

    Configure for Github pages

--- app.config.ts
+++ app.config.ts
@@ -1,3 +1,10 @@
 import { defineConfig } from "@solidjs/start/config";
+import staticAdapter from "solid-start-static";
 
-export default defineConfig({});
+export default defineConfig({
+  adapters: [
+    staticAdapter({
+      base: "/darts-ranking/",
+    }),
+  ],
+});
